ce2df62f05093a8e934c32a27be80ab3d688ca02
Mawunyo Hafoba <mawunyohafoba28@gmail.com>
2025-08-17 07:57:11 +0000

    Implemented btn and display

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 
-let lhs = undefined;
-let rhs = undefined;
+let lhs = 0;
+let rhs = 0;
 let operator = undefined;
+
+const buttons = document.querySelectorAll(".num");
+const label = document.querySelector("label");
+const clearBtn = document.querySelector('.clear');
+const signs = document.querySelectorAll(".sign");
+const dotBtn = document.querySelector(".dot");
+
+dot.addEventListener('click', () => {
+
+})
+
+
+clearBtn.addEventListener("click", (event) => {
+  clearDisplay(label);
+  event.stopImmediatePropagation();
+})
 
 function add(a, b) {
   return a + b;
@@ -43,3 +59,61 @@ function operate(a, operator, b) {
       break;
   }
 }
+
+function updateDisplay(display) {
+  display.textContent = `${lhs != undefined ? lhs : ''} ${operator != undefined ? operator : ''} ${operator === undefined && rhs === 0 ? "" : rhs}`
+}
+
+function clearDisplay(display) {
+  lhs = 0;
+  rhs = 0;
+  operator = undefined;
+  updateDisplay(display);
+}
+
+
+buttons.forEach((btn) => {
+  let num = Number(btn.textContent);
+  if (Number.isInteger(num)) {
+    btn.addEventListener('click', () => {
+      if (operator === undefined) {
+        lhs = lhs * 10 + num;
+      }
+      else if (operator != undefined) {
+        rhs = rhs * 10 + num;
+      }
+
+      updateDisplay(label);
+    })
+  }
+});
+
+
+signs.forEach((sign) => {
+  let op = sign.textContent;
+  op = op.trim();
+  sign.addEventListener('click', () => {
+    switch (op) {
+      case '/':
+        operator = '/';
+        break;
+      case 'x':
+        operator = '*';
+        break;
+      case '-':
+        operator = '-';
+        break;
+      case '%':
+        operator = "%";
+        break;
+      case "+":
+        operator = '+';
+      default:
+        break;
+    }
+
+    updateDisplay(label);
+  })
+});
+
+
